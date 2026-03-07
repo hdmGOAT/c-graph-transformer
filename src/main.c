cb3939bb7e7748c6044e410d1993db3b3a134589
Hans Matthew E. Del Mundo <delmundo.hansmatthew@gmail.com>
2026-03-07 21:29:05 +0800

    cleaned up comments

--- src/main.c
+++ src/main.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
-#include <math.h>
 #define E 7
 #define N 3
 #define FN 2
@@ -22,14 +21,7 @@ int main(void) {
 	    0.0f, 1.0f, 1.0f
 	};
 
-    	// Okay so i have 3 nodes with features being represented by two floating point values.
-    	// We wanna matmul our FEATURE_NUM * N matrix (features by node) to some learnable weight matrix,
-	// learnable weight matrix will be of FEATURE_NUM by Embedding length (lets say 16), BUT we should only
-	// do that operation towards neighbors, this is apparantly applied by multiplying against ajacency matrix
-	// then finally feed into an activation function, this output then becomes a node embedding    
-	// Use large context for easyness	
 	size_t ctx_size = 8 * 1024 * 1024; 
-	// STEP 0: set up ggml	
 	
 	struct ggml_init_params params = {
 		ctx_size, NULL, false
@@ -37,7 +29,6 @@ int main(void) {
 	
 	struct ggml_context *ctx = ggml_init(params);
 
-	// Step 0,5 : allocate tensors
 	struct ggml_tensor *feature_mat = ggml_new_tensor_2d(ctx, GGML_TYPE_F32, N, FN);	
 	memcpy(feature_mat->data, features, ggml_nbytes(feature_mat));
 	struct ggml_tensor *weight_mat = ggml_new_tensor_2d(ctx, GGML_TYPE_F32, FN, EMBEDDINGS_DEPTH);	
@@ -53,29 +44,21 @@ int main(void) {
 
 	struct ggml_tensor *adj_mat = ggml_new_tensor_2d(ctx, GGML_TYPE_F32, N, N);	
 	memcpy(adj_mat->data, adjacency, ggml_nbytes(adj_mat));
-
-
-	//  byuild graph
 	
 	struct ggml_cgraph *gf = ggml_new_graph(ctx);
 
-	// AX = A · X
 	struct ggml_tensor *ax =
 	    ggml_mul_mat(ctx, feature_mat, adj_mat);
 
-	// AXW = (A · X) · W
 	struct ggml_tensor *axw =
 	    ggml_mul_mat(ctx, weight_mat, ax);
 
-	// activation
 	struct ggml_tensor *out =
 	    ggml_relu(ctx, axw);
 
-	// build & run graph
 	ggml_build_forward_expand(gf, out);
 	ggml_graph_compute_with_ctx(ctx, gf, 1);
 
-	// print output (ggml column-major layout)
 	float *out_data = (float *) out->data;
 
 	printf("Node embeddings:\n");
@@ -87,6 +70,5 @@ int main(void) {
 	    printf("\n");
 	}
 
-	//I need to implement proper attention
     	return 0;
 }
